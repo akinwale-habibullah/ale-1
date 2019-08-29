d59ef216335a9722ceda7464b6a5cff576612992
Ayinla Olajide <ayinlaolajide@yahoo.com>
2019-08-29 23:39:26 +0100

    Implement get and edit user details - Add endpoint to get single user details - Add endpoint to edit single user details - Add endpoint to get all member details

--- models/user.js
+++ models/user.js
@@ -12,6 +12,7 @@ const User = sequelize.define('user', {
   email: { type: Sequelize.STRING, notNull: true },
   userName: { type: Sequelize.STRING, notNull: true },
   password: { type: Sequelize.STRING, notNull: true },
+  isAdmin: { type: Sequelize.BOOLEAN, notNull: true },
   joinDate: { type: Sequelize.STRING },
   memberId: { type: Sequelize.STRING, notNull: true },
   nextSavingsDate: { type: Sequelize.STRING },
@@ -20,7 +21,7 @@ const User = sequelize.define('user', {
   phone: { type: Sequelize.STRING },
   status: { type: Sequelize.STRING },
   isGuarantor: { type: Sequelize.BOOLEAN },
-  
+
 });
 
 /**
@@ -39,8 +40,8 @@ const User = sequelize.define('user', {
  * @param status The status of the candidate user
  * 
  */
-User.getOrCreateUser = function (firstName, lastName, email, userName, password, joinDate, memberId, nextSavingsDate, nextSavingsAmount, position, phone, status, isGuarantor) {
-  return User.findOrCreate({ where: { email }, defaults: { firstName, lastName, userName, password, joinDate, memberId, nextSavingsDate, nextSavingsAmount, position, phone, status, isGuarantor } })
+User.getOrCreateUser = function (firstName, lastName, email, userName, password, isAdmin, joinDate, memberId, nextSavingsDate, nextSavingsAmount, position, phone, status, isGuarantor) {
+  return User.findOrCreate({ where: { email }, defaults: { firstName, lastName, userName, password, isAdmin, joinDate, memberId, nextSavingsDate, nextSavingsAmount, position, phone, status, isGuarantor } })
     .then(result => {
       if (!result || result.length != 2) {
         return sequelize.Promise.reject(new AleError('User query failed to return expected result', codes.DatabaseQueryError));
@@ -68,6 +69,95 @@ User.getUserByAuth = function (email, password) {
       }
       return result;
     });
+};
+
+/**
+ * Gets details for an existing user
+ * @param userId The name of the user
+ */
+User.getUserDetails = async function (userId) {
+  try {
+    const result = await User.find({ attributes: { exclude: ['password'] }, where: { id: userId } });
+    if (!result) {
+      return sequelize.Promise.reject(new AleError('User with id does not exist', codes.DatabaseQueryError));
+    }
+    return result;
+  } catch (error) {
+    return sequelize.Promise.reject(new AleError(error, codes.DatabaseQueryError));
+  }
+};
+
+
+
+/**
+ * Update details for an existing user
+ * @param userEmail The name of the user
+ */
+User.updateDetails = async function (
+  userEmail,
+  newFirstName,
+  newLastName,
+  newJoinDate,
+  newMemberId,
+  newNextSavingsDate,
+  newNextSavingsAmount,
+  newPosition,
+  newPhone,
+  newStatus,
+  newIsGuarantor,
+) {
+  try {
+
+    const priorDetails = await User.find({ where: { email: userEmail } })
+    const newIsGuarantorObj = { isGuarantor: newIsGuarantor }
+    const result = await User.update(
+      {
+        firstName: newFirstName || priorDetails.firstName, lastName: newLastName || priorDetails.lastName,
+        joinDate: newJoinDate || priorDetails.joinDate, memberId: newMemberId || priorDetails.memberId,
+        nextSavingsDate: newNextSavingsDate || priorDetails.nextSavingsDate, nextSavingsAmount: newNextSavingsAmount || priorDetails.nextSavingsAmount,
+        position: newPosition || priorDetails.position, phone: newPhone || priorDetails.phone,
+        status: newStatus || priorDetails.status, isGuarantor: newIsGuarantorObj["isGuarantor"] === undefined ? priorDetails.isGuarantor : newIsGuarantorObj["isGuarantor"],
+      },
+      {
+        where: { email: userEmail },
+        returning: true,
+        plain: true
+      }
+    );
+
+    if (!result) {
+      return sequelize.Promise.reject(new AleError('Unable to update record at this time', codes.DatabaseQueryError));
+    }
+    return result[1];
+  } catch (error) {
+    return sequelize.Promise.reject(new AleError(error, codes.DatabaseQueryError));
+  }
+};
+
+
+/**
+ * Gets details of all users
+ * @param userId The name of the user
+ */
+User.getAllDetails = async function (isAdmin) {
+  try {
+    let allUsers;
+    if (isAdmin) {
+      allUsers = User.findAll({
+        attributes: { exclude: ['password', 'createdAt', 'updatedAt'] }
+      });
+    } else {
+      allUsers = User.findAll({
+        attributes: ['id', 'firstName', 'lastName', 'memberId']
+      });
+    }
+    if (!allUsers) {
+      return sequelize.Promise.reject(new AleError('Unable to get all users', codes.DatabaseQueryError));
+    }
+    return allUsers;
+  } catch (error) {
+    return sequelize.Promise.reject(new AleError(error, codes.DatabaseQueryError));
+  }
 };
 
 module.exports = User;
